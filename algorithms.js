f3d8b99bfb38f4b786b573f32c4e51d6f70ae8b1
alexkimeu0 <kim3ual3x@gmail.com>
2020-03-30 20:08:22 +0300

    day 1

--- algorithms.js
+++ algorithms.js
@@ -100,6 +100,26 @@ function longestWord(str) {
 }
 
 
+/* ========================================== */
+/* Title Case a sentence */
+function titleCase(str) {
+    var words = str.toLowerCase().split(" ");
+
+    for (var i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+    return words.join(" ");
+}
+
+
+/* Title Case a sentence */
+function titleCase(str) {
+    var titled = str.toLowerCase().split(" ").map(function (elem) {
+        return elem[0].toUpperCase() + elem.slice(1);
+    });
+    return titled.join(" ");
+}
+
 
 
 
